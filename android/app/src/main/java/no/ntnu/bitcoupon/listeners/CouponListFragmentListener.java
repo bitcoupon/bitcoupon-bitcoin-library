8c9220d6d379e77d1da0fb32fe21c8687aa7cef2
Patrick <mail@habitats.no>
2014-09-22 17:07:05 +0200

    basic main activity implementation
    The main activity acts as an entry point for the application, and holds
    the fragments. It should handle all communication between the fragments
    relevant to this activity. For the time being this is limited to the
    coupon fragment and the list of coupons.

---
+++ android/app/src/main/java/no/ntnu/bitcoupon/listeners/CouponListFragmentListener.java
@@ -0,0 +1,11 @@
+package no.ntnu.bitcoupon.listeners;
+
+import no.ntnu.bitcoupon.models.Coupon;
+
+/**
+ * Created by Patrick on 22.09.2014.
+ */
+public interface CouponListFragmentListener {
+
+  void onCouponClicked(Coupon id);
+}
