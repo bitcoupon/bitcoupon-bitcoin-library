0d5f64e82887f2f64650a443b722a13f276aa0e0
Patrick <mail@habitats.no>
2014-09-22 17:05:34 +0200

    created a basic coupon list fragment, to show a list of coupons

---
+++ android/app/src/main/java/no/ntnu/bitcoupon/fragments/CouponListFragment.java
@@ -0,0 +1,95 @@
+package no.ntnu.bitcoupon.fragments;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import no.ntnu.bitcoupon.R;
+import no.ntnu.bitcoupon.adapters.CouponListAdapter;
+import no.ntnu.bitcoupon.listeners.CouponListFragmentListener;
+import no.ntnu.bitcoupon.models.Coupon;
+
+public class CouponListFragment extends BaseFragment implements AbsListView.OnItemClickListener {
+
+
+  private no.ntnu.bitcoupon.listeners.CouponListFragmentListener mListener;
+  private AbsListView couponList;
+  private CouponListAdapter couponAdapter;
+
+  public static Fragment newInstance() {
+    CouponListFragment fragment = new CouponListFragment();
+    return fragment;
+  }
+
+  /**
+   * Mandatory empty constructor for the fragment manager to instantiate the fragment (e.g. upon screen orientation
+   * changes).
+   */
+  public CouponListFragment() {
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    if (getArguments() != null) {
+      // set arguments if there are any
+    }
+
+    couponAdapter = new CouponListAdapter(getActivity());
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_coupon_list, container, false);
+
+    View generateButton = view.findViewById(R.id.b_generate);
+    generateButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        couponAdapter.add(Coupon.createDummy());
+        couponAdapter.notifyDataSetChanged();
+      }
+    });
+    // Set the adapter
+    couponList = (ListView) view.findViewById(R.id.coupon_list);
+    couponList.setAdapter(couponAdapter);
+    couponList.setEmptyView(view.findViewById(R.id.empty));
+
+    // Set OnItemClickListener so we can be notified on item clicks
+    couponList.setOnItemClickListener(this);
+
+    return view;
+  }
+
+  @Override
+  public void onAttach(Activity activity) {
+    super.onAttach(activity);
+    try {
+      mListener = (no.ntnu.bitcoupon.listeners.CouponListFragmentListener) activity;
+    } catch (ClassCastException e) {
+      throw new ClassCastException(
+          activity.toString() + " must implement " + CouponListFragmentListener.class.getSimpleName());
+    }
+  }
+
+  @Override
+  public void onDetach() {
+    super.onDetach();
+    mListener = null;
+  }
+
+
+  @Override
+  public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    if (null != mListener) {
+      mListener.onCouponClicked(couponAdapter.getItem(position));
+    }
+  }
+}
