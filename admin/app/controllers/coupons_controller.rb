a783cfe0d5bf3bb7301f0366c9c4f49bedd8928a
Kristian Våge <krisvaage@gmail.com>
2014-09-23 12:45:02 +0200

    Remove binding.pry

--- admin/app/controllers/coupons_controller.rb
+++ admin/app/controllers/coupons_controller.rb
@@ -1,7 +1,6 @@
 class CouponsController < ApplicationController
   def consume
     result = Net::HTTP.get(URI.parse('http://localhost:3002/backend/coupons.json'))
-    binding.pry
     @coupons = JSON.parse(result)
   end
 end
