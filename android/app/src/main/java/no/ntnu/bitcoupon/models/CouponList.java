5d7f9e5f3310ba4d9981a155a1c4b71e99e466a8
Patrick <mail@habitats.no>
2014-09-23 16:27:15 +0200

    documentation for couponlist

--- android/app/src/main/java/no/ntnu/bitcoupon/models/CouponList.java
+++ android/app/src/main/java/no/ntnu/bitcoupon/models/CouponList.java
@@ -7,8 +7,12 @@ import java.util.List;
 
 /**
  * Created by Patrick on 23.09.2014.
+ *
+ * Model class to hold a list of coupons. Used by gson to convert a dictionary of coupons in json into a list of coupons
+ * in java
  */
 public class CouponList {
+
   List<Coupon> coupons;
 
   public static CouponList fromJson(BufferedReader reader) {
