706c29a508bef9425dcf1373421be10bb7ecca88
Kristian Våge <krisvaage@gmail.com>
2014-09-28 16:34:57 +0200

    Creates .sh files for before and after script instead of having them inlined

---
+++ .before_travis.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if [ $ADMIN == true ]; then
+  cd backend
+  bundle exec bin/rails s -p 8080 &
+  pid=$!
+  export RAILSPID=$pid
+  echo "pid - $pid"
+  echo "rails pid - $RAILSPID"
+  cd ..
+  sleep 5
+  echo "Started rails server for backend";
+else
+  echo "Did not start backend server";
+fi
+
+if [ "$ADMIN" == true ]; then
+  cd admin
+  echo "cd into admin";
+else
+  echo "not cd into admin";
+fi
+
+if [ "$BACKEND" == true ]; then
+  cd backend
+  echo "cd into backend";
+else
+  echo "not cd into backend";
+fi
