e8bb2cfc3fc17da4e0596a5ad2e7fc1816e89151
Orienterik <erik@himle.st>
2014-09-30 13:22:09 +0200

    New class BitCoupon

---
+++ bitcoin/src/main/java/bitcoupon/BitCoupon.java
@@ -0,0 +1,18 @@
+package bitcoupon;
+
+import org.apache.commons.codec.binary.Hex;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class BitCoupon {
+
+  public static Transaction generateCreationTransaction(String strPrivateKey) {
+
+  }
+
+  public static Transaction generateSendTransaction(String privateKey, String creatorPublicKey, String address, String transactionHistoryJson) {
+
+  }
+
+}
