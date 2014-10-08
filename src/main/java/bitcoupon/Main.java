96998198550036cbdb0723bcbc10a22c2cf8b580
MatsFisk <matsjoru@stud.ntnu.no>
2014-10-08 12:36:47 +0200

    Documentation in Main.java

--- src/main/java/bitcoupon/Main.java
+++ src/main/java/bitcoupon/Main.java
@@ -7,12 +7,32 @@ public class Main {
   private static final String GET_CREATOR_ADDRESSES = "getCreatorAddresses";
   private static final String VERIFY_TRANSACTION = "verifyTransaction";
 
+
+  /**
+   * This method takes a String array. It is passed on to evaluateMethod(args).
+   */
   public static void main(String[] args) {
     evaluateMethod(args);
 
 //    testTransaction();
   }
 
+  /**
+   * This method takes a String array. args[0] contains the method the programs that calls the library wishes to use.
+   * These methods include:
+   *
+   * GENERATE_CREATION_TRANSACTION
+   *
+   * GENERATE_SEND_TRANSACTION
+   *
+   * GET_CREATOR_ADDRESSES
+   *
+   * VERIFY_TRANSACTION <p/>
+   *
+   * The rest of the tuples contains the argument for the corresponding method. This method reads which method is being
+   * called, runs isValidArgumentsLength(), and if that checks out, sends the arguments to that method. If args[] is
+   * empty, the method returns a list of available methods.
+   */
   private static void evaluateMethod(String[] args) {
     if (args.length == 0) {
       System.out.println("Available methods:");
@@ -27,7 +47,9 @@ public class Main {
 
       String methodName = args[0];
 
-      checkArgumentsLength(methodName, args.length);
+      if (!isValidArgumentsLength(methodName, args.length)) {
+        return;
+      }
 
       if (methodName.equalsIgnoreCase(GENERATE_CREATION_TRANSACTION)) {
         BitCouponCli.generateCreationTransaction(args[1]);
@@ -41,20 +63,33 @@ public class Main {
     }
   }
 
-  private static void checkArgumentsLength(String methodName, int argsLength) {
+  /**
+   * This method takes the name of the method, runs checkLength() to see if the number of arguments corresponds to what
+   * the method needs, and returns true or false depending on whether it's correct or not. If it's incorrect, it will print out an error message.
+   * @param methodName
+   * @param argsLength
+   * @return
+   */
+  private static boolean isValidArgumentsLength(String methodName, int argsLength) {
 
     boolean correct = checkLength(methodName, argsLength);
     if (!correct) {
       String message = "Wrong number of arguments to method: " + methodName;
+      System.err.println(message + methodName);
+      return false;
 
-      // TODO Choose between exception or exit with failure.
-      //throw new IllegalArgumentException("Wrong number of arguments to method: " + methodName);
-
-      System.err.println("Wrong number of arguments to method: " + methodName);
-      System.exit(1);
     }
+    return true;
   }
 
+  /**
+   * This method gets the name of a method and an integer representing the numbers of arguments sent as input to that method,
+   * and checks that the number of arguments matches the number of arguments the method demands. It returns true if it matches, and false if it doesn't.
+   * 
+   * @param methodName
+   * @param argsLength
+   * @return
+   */
   private static boolean checkLength(String methodName, int argsLength) {
     int length = 0;
     if (methodName.equalsIgnoreCase(GENERATE_CREATION_TRANSACTION)) {
