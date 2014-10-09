8c3e9c40694d3d088822a66fd7bd7fb85694d1d0
HakonHesselberg <hakon.hesselberg@gmail.com>
2014-10-09 17:59:00 +0200

    Added CreationTest

---
+++ src/test/java/bitcoupon/transaction/CreationTest.java
@@ -0,0 +1,27 @@
+package bitcoupon.transaction;
+
+import junit.framework.TestCase;
+
+public class CreationTest extends TestCase {
+
+  public void testConstructor() throws Exception {
+    // TestConstructorXxx where Xxx is NullCreatorAddress, NegativeAmount etc.
+    // Should throw illegalArgumentException
+  }
+
+  public void testGetBytes() throws Exception {
+
+  }
+
+  public void testGetCreatorAddress() throws Exception {
+
+  }
+
+  public void testGetAmount() throws Exception {
+
+  }
+
+  public void testGetSetSignature() throws Exception {
+    
+  }
+}
